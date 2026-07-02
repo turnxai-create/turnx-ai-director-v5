728efc1a6abdb5499c1ba00a42ae1d88b23d7558
turnxai-create <turnxai@gmail.com>
2026-07-02 14:56:22 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "")
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
